72bc7dc5fe55a68456a39e746f79c4a9b4ae51fa
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-05-22 09:15:41 +0000

    Add existing file

--- ArrayReverse.cpp
+++ ArrayReverse.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    cout << "Hello world!" << endl;
+    vector<int> nums;
+    int a, i;
+
+    cout << "Enter anything other than a number to terminate input." << endl;
+
+    while(cin >> a){
+        nums.push_back(a);
+    }
+
+    cout << "{";
+
+    for(i = nums.size() - 1; i >= 0; i--){
+        cout << nums[i] << " ";
+    }
+
+    cout << "}\n";
 }
