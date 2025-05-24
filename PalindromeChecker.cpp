452920c2cb6802e58d637305c9633bf501bf7ffc
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-05-24 02:31:29 +0000

    PalindromeChecker

---
+++ PalindromeChecker.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+//Check if a given string is an exact palindrome or not
+bool isPalindrome(string s){
+
+    bool flag = true;
+
+    for(int i = 0; i < s.length() / 2; i++){
+        if(s[i] != s[s.length() - 1 - i]){
+            flag = false;
+        }
+    }
+
+    return flag;
+}
+
+int main(){
+
+    //Change this to the string you want to check is a palindrome
+    string pal = "madam";
+
+    //1: it is a palindrom, 0: it is not a palindrome
+    cout << isPalindrome(pal) << endl;
+    return 0;
+}
