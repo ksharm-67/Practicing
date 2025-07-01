49b875e389836df86ef05971330bf4210a9df19c
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-07-01 07:08:36 +0000

    Power of 3

---
+++ PowerOf3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+
+//Check if the given number is a power of 3 or not
+bool isPowerOfThree(int n) {
+    if (n <= 0) return false;
+
+    double num = log10(n) / log10(3);
+    
+    return (floor(num) == num) && (ceil(num == num));
+}
