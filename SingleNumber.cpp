6c00d48a2b72d64ec8f9f4d6e8530f19b5194ec1
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-07-12 23:14:10 -0700

    Return the only element that occurs once

--- SingleNumber.cpp
+++ SingleNumber.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> mp;
 
-        if(nums.size() == 1) return nums[0];
-
-        unordered_map<int, vector<int>> mp;
+        if (nums.size() == 1) return nums[0];
 
         for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]].push_back(nums[i]);
+            
+            auto itr = mp.find(nums[i]);
+
+            if(itr != mp.end()){
+                mp[nums[i]] += 1;
+            }
+            else mp[nums[i]] = 1;
+
         }
 
-        for(auto itr : mp){
-            if (itr.second.size() == 1) return itr.second[0];
+        for(auto m : mp){
+            cout << m.first << " : " << m.second << endl;
+            if (m.second == 1) return m.first;
         }
+
         return 0;
     }
 };
