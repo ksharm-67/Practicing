93a25e1caf0f62754c688a8ed41dff573992d2c4
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-08-08 04:13:13 +0000

    Sorting the sentence

---
+++ SortingSentence.cpp
@@ -0,0 +1,15 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        
+        L = s.split()
+        sortedList = [i for i in range(len(L))]
+        
+        for i in L:
+            index = int(i[-1])
+            sortedList[index - 1] = i[:-1]
+        
+        res = ""
+        for j in sortedList:
+            res = res + j + " "
+
+        return res[:-1]
