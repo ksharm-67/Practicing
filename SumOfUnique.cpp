c2630c234dc24c788e1a334a0851b2c1645746d4
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-08-15 10:24:53 +0000

    Sum of unique elements

---
+++ SumOfUnique.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+
+        int sum = 0;
+        for(auto m : mp){
+            if(m.second == 1){
+                sum += m.first;
+            }
+        }
+
+        return sum;
+    }
+};
