ca656a2d9d3781535c0ff412f038e9c55427a560
ksharm-67 <148153826+ksharm-67@users.noreply.github.com>
2025-10-25 01:31:08 -0700

    Minimum Operations to Exceed Threshold Value II

---
+++ MinOpsThrshld.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        
+        heapq.heapify(nums)
+
+        ops = 0
+        while nums[0] < k:
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+
+            heapq.heappush(nums, min(x, y) * 2 + max(x, y))
+            ops += 1
+
+        return ops
